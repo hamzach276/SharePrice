7c379d07bbf2aeb7e7e37d62922e26b58b7510f6
Hamza Chaudhary <31058254+hamzach276@users.noreply.github.com>
2025-07-17 16:25:23 +0500

    Init

---
+++ SharePrice.ps1
@@ -0,0 +1,1 @@
+﻿Invoke-WebRequest -Uri "https://tools.eurolandir.com/tools/pricefeed/?companycode=ae-masq&format=json" -OutFile "E:\Genial365 All Projects\SharePrice\download$(Get-Date -Format yyyyMMdd_HHmmss).txt"
